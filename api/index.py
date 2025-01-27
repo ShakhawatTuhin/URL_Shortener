fc17e8ef84270c3ae1ebacc51883569b5d08f0e3
ShakhawatTuhin <shakawattuhin3@gmail.com>
2025-01-27 22:54:10 +0100

    .env file added

--- api/index.py
+++ api/index.py
@@ -1,17 +1,19 @@
+import os
 import random
 import sqlite3
 import string
-
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__, template_folder='../templates')
 
+# Get the database path from an environment variable, or use a default path
+DATABASE_PATH = os.getenv('DATABASE_PATH', '../database.db')
+
 def generate_short_url():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
 
-
 def init_db():
-    conn = sqlite3.connect('../database.db')
+    conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS urls(
@@ -22,6 +24,7 @@ def init_db():
     """)
     conn.commit()
     conn.close()
+
 init_db()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,25 +38,22 @@ def index():
 
         short_url = generate_short_url()
 
-
-        conn = sqlite3.connect('../database.db')
+        conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
         cursor.execute("INSERT INTO urls (original_url, short_url) VALUES (?, ?)", (original_url, short_url))
         conn.commit()
         conn.close()
 
-        return render_template('result.html', short_url= short_url)
+        return render_template('result.html', short_url=short_url)
     return render_template('index.html')
-
 
 @app.route('/<short_url>')
 def redirect_to_url(short_url):
-    conn = sqlite3.connect('../database.db')
+    conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
     cursor.execute("SELECT original_url FROM urls WHERE short_url = ?", (short_url,))
     result = cursor.fetchone()
     conn.close()
-
 
     if result:
         return redirect(result[0])
@@ -61,5 +61,4 @@ def redirect_to_url(short_url):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
